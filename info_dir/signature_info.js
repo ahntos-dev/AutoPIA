e8d74196fc3389140bfb39645e162146864ff665
Ahmad Joyner <82827608+ahntos-dev@users.noreply.github.com>
2021-09-08 11:24:06 -0400

    Create signature_info.js
    Stores all signature locations for usage in pia.js

---
+++ info_dir/signature_info.js
@@ -0,0 +1,6 @@
+const signature = {
+    FIRST_NAME: 'ctl00_ContentCenter_ElectronicSignature1_FirstName',
+    LAST_NAME: 'ctl00_ContentCenter_ElectronicSignature1_LastName',
+    TODAYS_DATE: 'ctl00_ContentCenter_ElectronicSignature1_Today',
+};
+module.exports = { signature };
